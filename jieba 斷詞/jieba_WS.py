4bb812ab25d4b4af55bb593281067c89b19a3f0b
Jason901008 <122069828+Jason901008@users.noreply.github.com>
2024-01-30 22:07:41 +0800

    Update jieba_WS.py

--- jieba 斷詞/jieba_WS.py
+++ jieba 斷詞/jieba_WS.py
@@ -2,8 +2,7 @@ import jieba
 import pandas as pd
 
 
-
-pred_risk_file = r'Result_Record/Public_Danger/ner/Public_Danger_interface_Pred_all_risk.csv'
+pred_risk_file = r'interface_Pred_all_risk/Drug_interface_Pred_all_risk.csv'
 user_dict_file = r'Ws_Dictionary.txt'
 
 
@@ -34,8 +33,7 @@ def Word_Span () :
         seg_list = jieba.cut ( pred_risk[i], cut_all=False, HMM=True )
         #for w in seg_list if word not in stop_words                        #去掉停用詞
         for w in seg_list :
-            result += w + " "
-            #ws.append ( w )    
+            result += w + " " 
         
         pred_WS.append ( result )
 
