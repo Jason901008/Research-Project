0df75c6d985933a2632da1718a20b032303e9ad0
Jason901008 <122069828+Jason901008@users.noreply.github.com>
2024-01-31 21:46:10 +0800

    Update NER_TEST.py

--- 風險詞/NER_TEST.py
+++ 風險詞/NER_TEST.py
@@ -3,9 +3,10 @@ import pandas as pd
 from ckip_transformers.nlp import CkipNerChunker
 
 
-ner_driver_4 = CkipNerChunker ( model_name="D:/NER_TEST/MODEL",device = 0 )
+#ner_driver_4 = CkipNerChunker ( model_name = "D:/Downloads/MODEL",device = 0 )
+ner_driver_4 = CkipNerChunker ( model_name = r"../../MODEL",device = 0 )
 
-csv_file_path = r"Result_Record/Negligent_Injury/Negligent_Injury_original_data.csv"
+csv_file_path = r"Result_Record/Drug_original_data.csv"
 
 df = pd.read_csv ( csv_file_path , header=None )
 test_data = df[0].tolist()
@@ -15,7 +16,7 @@ ner_4 = ner_driver_4 ( test_data )
 
 
 # Write & Read NER File
-with open ( r'Negligent_Injury_predict_ner.pkl', 'wb' ) as ner_file :
+with open ( r'Drug_predict_ner.pkl', 'wb' ) as ner_file :
     pickle.dump ( ner_4 , ner_file )
 
 with open( r'test_predict_ner.pkl', 'rb') as ner_file:
